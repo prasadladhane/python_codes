28b320da50e8eb77ccf1f5555a43f392ed666e21
prasadladhane <prasadladhane123@gmail.com>
2025-08-13 00:07:23 -0400

    Datatype completed

---
+++ datatypes/Program3.py
@@ -0,0 +1,17 @@
+x=3.14
+print(x,type(x))
+
+y="Prasad"
+print(y,type(y))
+
+z=[5,10,15,20,25,30]
+print(z, type(z))
+
+p=(2,4,6,8,10)
+print(p, type(p))
+
+q={3,6,9,12,15}
+print(p, type(q))
+
+r={"Virat":18,"MS":7,"Rohit":45,"Sachin":1}
+print(r, type(r))
---
+++ datatypes/Program4.py
@@ -0,0 +1,2 @@
+list=[1,1,5,7,3]
+print(list)
